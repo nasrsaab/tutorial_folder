7e5f90ccf0dff4fdd86ad0d6322eea9e1ce7e0ea
NasrEldin Saab-2022 <98010047+nasrsaab@users.noreply.github.com>
2022-07-06 11:19:15 +0300

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def fib(n :int):
+    a,b=0,1
+    while a < n:
+        print (a,end=' ')
+        a,b=b,a+b
+    print()
+
+def fib2(n: int)-> List[int]:
+    result = []
+    a,b=0,1
+    while a<n:
+        result.append(a)
+        a,b=b,a+b
+    return result
