c8933a6f99f3a565cf62ee851fbe06dce4183b03
NasrEldin Saab-2022 <98010047+nasrsaab@users.noreply.github.com>
2022-07-06 11:20:08 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='fiboProject',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='httpe://github.com',
+    license='',
+    author='nsaab',
+    author_email='nasr.saab@gmail.com',
+    description='this is a fibo example',
+    py_modules=['fibo']
+)
